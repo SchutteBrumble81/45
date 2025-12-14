cad542c51e88f86fe98aa36cdb0c34071fab14e8
SchutteBrumble81 <SchutteBrumble81@outlook.com>
2025-12-14 13:44:22 -0800

    Upload de 7a85d0222a1cc66477e44512ca945dc0.js

---
+++ 7a85d0222a1cc66477e44512ca945dc0.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>seu plano ou teste expirou (1).mp4</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<meta name="description" content="" />
+
+<meta property="og:url" content="https://iframe.mediadelivery.net/embed/499524/9488efb7-269e-47e7-b972-317945a134b7" />
+<meta property="og:image" content="https://vz-913b5db2-748.b-cdn.net/9488efb7-269e-47e7-b972-317945a134b7/thumbnail.jpg" />
+<meta property="og:image:secure_url" content="https://vz-913b5db2-748.b-cdn.net/9488efb7-269e-47e7-b972-317945a134b7/thumbnail.jpg" />
+<meta property="og:site_name" content="bunny.net" />
+<meta property="og:title" content="seu plano ou teste expirou (1).mp4" />
+<meta property="og:description" content="" />
+<meta property="og:type" content="video.other" />
+
+<meta property="og:video:url" content="https://vz-913b5db2-748.b-cdn.net/9488efb7-269e-47e7-b972-317945a134b7/play_720p.mp4" />
+<meta property="og:video:secure_url" content="https://vz-913b5db2-748.b-cdn.net/9488efb7-269e-47e7-b972-317945a134b7/play_720p.mp4" />
+
+<meta property="og:video:type" content="text/html" />
+<meta property="og:video:width" content="1920">
+<meta property="og:video:height" content="1080">
+<meta property="video:duration" content="285">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="player">
+<meta property="twitter:domain" content="bunny.net">
+<meta property="twitter:url" content="https://iframe.mediadelivery.net/embed/499524/9488efb7-269e-47e7-b972-317945a134b7">
+<meta name="twitter:title" content="seu plano ou teste expirou (1).mp4">
+<meta name="twitter:description" content="">
+<meta name="twitter:image" content="https://vz-913b5db2-748.b-cdn.net/9488efb7-269e-47e7-b972-317945a134b7/thumbnail.jpg">
+<meta name="twitter:player" content="https://iframe.mediadelivery.net/embed/499524/9488efb7-269e-47e7-b972-317945a134b7">
+<meta name="twitter:player:width" content="1920">
+<meta name="twitter:player:height" content="1080">
+
+
+
+
+<script type="application/ld+json">
+    {
+        "@context": "https://schema.org",
+        "@type": "VideoObject",
+        "name": "seu plano ou teste expirou (1).mp4",
+        "description": "",
+        "thumbnailUrl": "https://vz-913b5db2-748.b-cdn.net/9488efb7-269e-47e7-b972-317945a134b7/thumbnail.jpg",
+        "uploadDate": "2025-09-24T11:10:46.9040000+00:00",
+        "duration": "PT4M45S",
+        "embedUrl": "https://iframe.mediadelivery.net/embed/499524/9488efb7-269e-47e7-b972-317945a134b7"
+    }
+</script>
+
+
+    <link rel="stylesheet" href="https://assets.mediadelivery.net/play.css">
+    <style>
+        .wrapper {
+            --video-aspect-ratio: 1;
+            --padding: 2em;
+            box-sizing: border-box;
+            width: calc((100vh - (2 * var(--padding))) * var(--video-aspect-ratio));
+            padding: var(--padding);
+            margin-top: -6px;
+            margin-left: auto;
+            margin-right: auto;
+            max-width: 1280px;
+        }
+    </style>
+</head>
+<body id="body">
+    <div class="wrapper" id="video-wrapper">
+        <div class="container">
+            <iframe src="https://iframe.mediadelivery.net/embed/499524/9488efb7-269e-47e7-b972-317945a134b7?autoplay=true" loading="lazy" style="border: none;" allowfullscreen="true" allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"></iframe>
+        </div>
+    </div>
+</body>
+</html>
